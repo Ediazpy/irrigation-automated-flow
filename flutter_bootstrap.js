a04c25ec65d587fca445e3055f3eac7a7a7851c8
Ediazpy <Ediazpy@users.noreply.github.com>
2026-02-15 23:46:23 +0000

    deploy: dc649667a4829a2a380de25010c9063b38bbe2f6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "569760349"
+    serviceWorkerVersion: "3157880303"
   }
 });
